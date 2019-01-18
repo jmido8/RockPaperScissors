e8212157dce2022d914725dc70383447f9b01141
Jason <jmido8@yahoo.com>
2019-01-18 12:23:18 +0800

    Add domStrings and gameVariables as modules

---
+++ domStrings.js
@@ -0,0 +1,16 @@
+    
+//Preps the clickable area to get your hand choice
+export const choiceUI = document.querySelector(".container");
+
+//Selects the score from html
+export const scoreUI = document.querySelector(".score");
+
+//Selects the results from html
+export const resultUI = document.querySelector(".results");
+
+//Selects area for opponent's rock/paper/scissors picture
+export const oppPic = document.querySelector(".oppHand");
+
+//Selects area for player's rock/paper/scissors picture
+export const myPic = document.querySelector(".myHand");
+
---
+++ gameVariables.js
@@ -0,0 +1,6 @@
+export let score = 0;
+export let rounds = 0;
+export let results = "New Game";
+export const rockPic = "\"https://images.pexels.com/photos/355863/pexels-photo-355863.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260\"";
+export const paperPic = "\"https://images.pexels.com/photos/388898/pexels-photo-388898.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260\"";
+export const scissorsPic = "\"https://images.pexels.com/photos/271402/pexels-photo-271402.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260\"";
